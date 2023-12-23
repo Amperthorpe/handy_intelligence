da1ce740713857e78c10fa8047a2ffd35764e639
Luna <luna@lunaskye.net>
2023-12-22 23:46:45 -0800

    impl logging call

--- config_handler.py
+++ config_handler.py
@@ -1,4 +1,8 @@
+import logging
 from configparser import ConfigParser
 
+logger = logging.getLogger(__name__)
+
+logger.log("Config Handler init")
 config = ConfigParser()
 config.read("config.cfg")
